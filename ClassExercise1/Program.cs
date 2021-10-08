22ef9a794b2f5d5245a908b6aa1465de37d67843
coffie2000us <charlescoffie.cri@gmail.com>
2021-10-08 15:04:21 -0400

    initial commit

---
+++ ClassExercise1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Honda";
+            myCar.Model = "Civic";
+            myCar.Year = 2020;
+
+            var toyota = new Car()
+            {
+                Make = "Toyota",
+                Model = "Corolla",
+                Year = 2018,
+            };
+
+            var kia = new Car("Kia", "Soul", 2019);
+           
+            var carList = new List<Car>() { myCar, toyota, kia};
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+        
+        
+        }   
+    }
+}
